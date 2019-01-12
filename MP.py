e16977fb0ca7d46304130d18732162c36a731763
Livii Iabanzhi <livannnn@yandex.ru>
2019-01-12 20:16:18 +0200

    backprop real

--- MP.py
+++ MP.py
@@ -1,26 +1,26 @@
-
 import pandas as pd
 import numpy as np
 
 class Multilayer_Perceptron(object):
-	def __init__(self, X, y, n_perceptrons=4, n_layers=2):
+	def __init__(self, X, y, n_perceptrons=4, n_layers=2, n_output=2):
 		self.X = X
 		self.y = y
 		self.y_name = np.unique(self.y)
 		self.n_p = n_perceptrons
 		self.n_l = n_layers
 		self.res = []
-		self.bias = np.random.random(n_layers + 1)
+		self.bias = []
 		self.alpha = 0.01
 
 		self.NN = []
-		self.output_layer = [0] * len(np.unique(self.y))
+		self.output_layer = [0] * len(self.y_name)
 
 		for i in range(len(self.output_layer)):
 			self.output_layer[i] = np.random.random(n_perceptrons)
 
 		for i in range(n_layers):
 			tmp = []
+			self.bias.append(pd.Series(np.random.random(n_perceptrons)))
 			for j in range(n_perceptrons):
 				if (i == 0):
 					tmp.append(np.random.random(len(self.X.columns)))
@@ -28,6 +28,8 @@ class Multilayer_Perceptron(object):
 					tmp.append(np.random.random(n_perceptrons))
 
 			self.NN.append(tmp)
+
+		self.bias.append(pd.Series(np.random.random(len(self.y_name))))
 
 	def scaling(self):
 		for i in self.X.columns:
@@ -55,25 +57,34 @@ class Multilayer_Perceptron(object):
 		for i in range(len(self.NN)):
 			tmp = []
 			for j in range(len(self.NN[i])):
-				tmp.append(self.sigmoid(self.res[i].dot(self.NN[i][j]) + self.bias[i]))
+				tmp.append(self.sigmoid(self.res[i].dot(self.NN[i][j]) + self.bias[i][j]))
 			self.res.append(pd.DataFrame(tmp).T)
 
 		last_layer = []
 
 		for i in range(len(self.y_name)):
-			tmp = self.sigmoid(self.res[-1].dot(self.output_layer[i]) + self.bias[-1])
-			last_layer.append(pd.DataFrame(tmp))
+			tmp = self.sigmoid(self.res[-1].dot(self.output_layer[i]) + self.bias[-1][i])
+			last_layer.append(pd.Series(tmp))
 
-		self.res.append(last_layer)
+		self.res.append(pd.DataFrame(last_layer).T)
 		self.NN.append(self.output_layer)
 
 	def backprop(self):
+		forNN = []
+		forBias = []
+		y = []
+
 		for i in range(len(self.y_name)):
-			y = pd.Series(np.where(self.y == self.y_name[i], 1, 0)) - self.res[-1][i]
-			for j in range(len(self.res) - 1, 1, -1):
-				o_k = y * self.sigmoid_derivative(self.res[j - 1].dot(self.NN[j - 1][i]) + self.bias[j - 1])
-				o_k = o_k[0]
+			tmp = np.where(self.y == self.y_name[i], 1, 0)
+			y.append(pd.Series(self.res[-1][i]) - pd.Series(tmp))
 
-				self.bias[j - 1] += self.alpha * sum(o_k)
+		forNN.append(pd.DataFrame(y))
 
-				
+		for i, nn in enumerate(reversed(self.NN)):
+			n = pd.DataFrame(nn).T
+			z = n.dot(forNN[i]).T
+
+			for j in range(len(z)):
+				z[j] = z[j] * self.sigmoid_derivative(self.res[(i + 3) * -1].dot(self.NN[(i + 2) * -1]) + self.bias[(i + 2) * -1][j])
+				nn[j] += sum(z[j]) * self.alpha
+
