d1ab392ab90ba2f019aac4d0a25a6a44b0ce4be6
livann <l.iabanzh@myrikki.ru>
2019-01-15 18:48:17 +0200

    backprop real

--- MP.py
+++ MP.py
@@ -10,7 +10,7 @@ class Multilayer_Perceptron(object):
 		self.n_l = n_layers
 		self.res = []
 		self.bias = []
-		self.alpha = 0.01
+		self.alpha = 0.0001
 
 		self.NN = []
 		self.output_layer = [0] * len(self.y_name)
@@ -52,6 +52,7 @@ class Multilayer_Perceptron(object):
 		return np.diagflat(s) - np.dot(s, np.transpose(s))
 
 	def prop(self):
+		self.res = []
 		self.res.append(self.X)
 
 		for i in range(len(self.NN)):
@@ -72,12 +73,12 @@ class Multilayer_Perceptron(object):
 	def backprop(self):
 		prev_error = []
 
+		print(self.NN)
+
 		for i in range(len(self.y_name)):
 			y = np.where(self.y == self.y_name[i], 1, 0)
 
 			prev_error.append(self.res[-1][i][0] - pd.Series(y))
-
-		# print(prev_error)
 
 		for i, nn in enumerate(reversed(self.NN)):
 			tmp = []
@@ -86,21 +87,15 @@ class Multilayer_Perceptron(object):
 				tmp.append(0.0)
 
 			for j in range(len(nn)):
-				o_k = prev_error[j] * self.sigmoid_derivative(self.res[(i + 2) * -1].dot(nn[j]) + self.bias[(i + 1) * -1])
+				o_k = prev_error[j] * self.sigmoid_derivative(self.res[(i + 2) * -1].dot(nn[j]) + self.bias[(i + 1) * -1][j])
 
 				for k in range(len(nn[j])):
-					w = self.alpha * o_k * self.res[(i + 2) * -1][k]
+					current = self.res[(i + 2) * -1]
+					w = self.alpha * o_k * current[current.columns[k]]
 					nn[j][k] += sum(w)
 					tmp[k] += sum(o_k * nn[j][k])
 
 			prev_error = tmp
 
-				# for k in range(len(prev_error)):
-
-			# for j in range(len(prev_error)):
-			# 	err = prev_error[j] * self.sigmoid_derivative(self.res[(i + 2) * -1].dot(nn[j]) + self.bias[(i + 1) * -1])
-			# 	err = pd.DataFrame(err)
-			# 	o_k = err.dot(pd.DataFrame(nn[j]).T)
-
-			# 	print(o_k)
+		print(self.NN)
 
